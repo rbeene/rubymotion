d8fcdf4ea24cffbe98709c9616171dd65bba0c86
Robert Beene <rbeene@teladoc.com>
2013-03-25 15:50:26 +0100

    first pass

--- Cocoaheads/app/app_delegate.rb
+++ Cocoaheads/app/app_delegate.rb
@@ -1,5 +1,9 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    next_event_view_controller = NextEventViewController.alloc.init
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.rootViewController = next_event_view_controller
+    @window.makeKeyAndVisible
     true
   end
 end
