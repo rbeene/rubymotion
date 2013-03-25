f846131f397d1bc57fece268bdb517420f255678
Robert Beene <rbeene@teladoc.com>
2013-03-25 14:14:26 +0100

    working estimation

--- estimator/app/app_delegate.rb
+++ estimator/app/app_delegate.rb
@@ -1,5 +1,12 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    project_view_controller = ProjectViewController.alloc.init
+    @window                 = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+
+    #Every window has a root view controller from which it will present its view
+    @window.rootViewController = project_view_controller
+    @window.makeKeyAndVisible
+
     true
   end
 end
