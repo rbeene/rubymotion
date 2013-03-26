99b97f844aefdd7eb58535c9aa620c229fcd30d9
Robert Beene <rbeene@teladoc.com>
2013-03-26 11:46:15 +0100

    my_cocoa

--- my_cocoa/app/app_delegate.rb
+++ my_cocoa/app/app_delegate.rb
@@ -1,5 +1,11 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    event_detail_view_controller = EventDetailViewController.alloc.init
+    @window                      = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+
+    #Every window has a root view controller from which it will present its view
+    @window.rootViewController = event_detail_view_controller
+    @window.makeKeyAndVisible
     true
   end
 end
