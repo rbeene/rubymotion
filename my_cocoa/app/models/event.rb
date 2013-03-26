240b4f2b554345dd73abf8537eb5727c44b0af10
Robert Beene <rbeene@teladoc.com>
2013-03-26 11:49:21 +0100

    geolocate location

--- my_cocoa/app/models/event.rb
+++ my_cocoa/app/models/event.rb
@@ -1,6 +1,6 @@
 class Event
 
-  attr_accessor :name, :address, :location, :date_as_text, :talks
+  attr_accessor :name, :address, :location, :date_as_text, :talks, :longitude, :latitude
 
   CUPERTINO_LOCATION = {:latitude => 37.334815, :longitude => -122.029781}
 
@@ -8,11 +8,21 @@ class Event
 
     mock_event              = Event.new
     mock_event.name         = "November meeting"
-    mock_event.address      = "Main St 107, Westlake, Seatle MA"
+    mock_event.address      = "8 Spruce Street, New York, NY 10038"
     mock_event.date_as_text = "Sat 14th November 2012"
-    mock_event.location     = CLLocationCoordinate2DMake( CUPERTINO_LOCATION[:latitude], CUPERTINO_LOCATION[:longitude] )
+    mock_event.set_coordinates
     mock_event.talks        = [Talk.mock_talk, Talk.mock_talk]
     mock_event
+  end
+
+  def set_coordinates
+    completion_block = lambda do |placemark, error|
+      coordinates = placemark[0].location.coordinate
+      @latitude = coordinates.latitude
+      @longitude = coordinates.longitude
+      self.location = CLLocationCoordinate2DMake(latitude, longitude)
+    end
+    CLGeocoder.alloc.init.geocodeAddressString(self.address, completionHandler: completion_block)
   end
 
 end
