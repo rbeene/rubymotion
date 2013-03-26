c3345b5b12a956636b60b36d7c27777e6b90c09f
Robert Beene <rbeene@teladoc.com>
2013-03-26 18:38:37 +0100

    chapter 12 and ruby motion slides

--- 12-table_views/app/controllers/news_view_controller.rb
+++ 12-table_views/app/controllers/news_view_controller.rb
@@ -45,11 +45,54 @@ class NewsViewController < UITableViewController
   def tableView(tableView, viewForHeaderInSection:section)
     header_view = UIImageView.alloc.initWithImage(UIImage.imageNamed("bgTitleBar"))
     header_view.frame = [[0,0], [320, 44]]
+    header_view.addSubview(deleteButton)
+    header_view.addSubview(addButton)
     header_view.setUserInteractionEnabled(true)
     header_view
   end
 
   def tableView(tableView, heightForHeaderInSection:section)
     64.0
+  end
+
+  def deleteButton
+    delete_button = UIButton.buttonWithType(UIButtonTypeCustom)
+    delete_button.setTitle("Delete", forState:UIControlStateNormal)
+    delete_button.frame = [[10, 12], [56, 41]]
+    delete_button.setBackgroundImage(UIImage.imageNamed("btnBarRed"), forState:UIControlStateNormal)
+    delete_button.addTarget(self,
+      action:"delete_selected_cell",
+      forControlEvents:UIControlEventTouchUpInside)
+    delete_button
+  end
+
+
+  def addButton
+    add_button = UIButton.buttonWithType(UIButtonTypeCustom)
+    add_button.setTitle("Add", forState:UIControlStateNormal)
+    add_button.frame = [[250, 12], [56, 41]]
+    add_button.setBackgroundImage(UIImage.imageNamed("btnBarRed"), forState:UIControlStateNormal)
+    add_button.addTarget(self,
+      action:"add_cell",
+      forControlEvents:UIControlEventTouchUpInside)
+    add_button
+  end
+
+
+  def delete_selected_cell
+    selected_cell_index_path = self.tableView.indexPathForSelectedRow
+    if selected_cell_index_path
+      news_item = @news[selected_cell_index_path.row]
+      @news.delete(news_item)
+      self.tableView.deleteRowsAtIndexPaths([selected_cell_index_path], withRowAnimation:UITableViewRowAnimationMiddle)
+    end
+  end
+
+
+  def add_cell
+    random_item = News.self.news_mock[(0..5).to_a.sample]
+    index_path = NSIndexPath.indexPathForRow(@news.length, inSection:0)
+    @news << random_item
+    self.tableView.insertRowsAtIndexPaths([index_path],withRowAnimation:UITableViewRowAnimationRight)
   end
 end
